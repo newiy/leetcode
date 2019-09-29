d29170447df39861f04e8f6bc7f22378702b2257
newiy <irene830@hotmail.com>
2019-09-28 20:15:36 -0700

    add new

---
+++ convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,47 @@
+//https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+// Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, two’s complement method is used.
+
+// Note:
+
+// All letters in hexadecimal (a-f) must be in lowercase.
+// The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single zero character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
+// The given number is guaranteed to fit within the range of a 32-bit signed integer.
+// You must not use any method provided by the library which converts/formats the number to hex directly.
+// Example 1:
+
+// Input:
+// 26
+
+// Output:
+// "1a"
+// Example 2:
+
+// Input:
+// -1
+
+// Output:
+// "ffffffff"
+
+#include <iostream>
+using namespace std;
+
+string toHex(int num) {
+    string res = "";
+    for(int i = 0; num && i < 8; i++) {
+        int t = num & 0xf;
+        if (t >= 10) {
+            res = char('a' + t - 10) + res;
+        } else {
+            res = char('0' + t) + res;
+        }
+        num >>= 4;
+    }
+    return res.empty() ? "0" : res;
+}
+
+int main() {
+  printf("%s\n", toHex(26).c_str());
+  printf("%s\n", toHex(0).c_str());
+  printf("%s\n", toHex(-1).c_str());
+	return 0;
+}
